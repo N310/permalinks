7693ea69817e1b0d36ff8f3d0039c29377fc4de1
Sato Nae <saton@cotegg.com>
2019-07-01 00:41:21 +0900

    全投稿のURL取得実装

--- app/controllers/scraping_controller.rb
+++ app/controllers/scraping_controller.rb
@@ -1,5 +1,35 @@
 class ScrapingController < ApplicationController
-  def new
+  # URLにアクセスするためのライブラリの読み込み
+  require 'open-uri'
+  # Nokogiriライブラリの読み込み
+  require 'nokogiri'
 
+  def new
+    charset = nil
+    page_no = 1
+    @url_data = []
+    # スクレイピング先のURL
+    url = "https://310nae.com/page/#{page_no}"
+
+    begin
+      while true
+        html = open(url) do |f|
+          charset = f.charset # 文字種別を取得
+          f.read # htmlを読み込んで変数htmlに渡す
+        end
+
+        # htmlをパース(解析)してオブジェクトを生成
+        doc = Nokogiri::HTML.parse(html, nil, charset)
+
+        post_links = doc.css(".post-list a")
+        post_links.each do |post_link|
+          @url_data.push(post_link.attributes["href"].value)
+        end
+
+        page_no += 1
+        url = "https://310nae.com/page/#{page_no}"
+      end
+    rescue OpenURI::HTTPError => error
+    end
   end
 end
