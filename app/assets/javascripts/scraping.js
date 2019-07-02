aa0b5368b18b4b6c0c9ad81abe5b13005a12cf07
Sato Nae <saton@cotegg.com>
2019-07-03 00:47:27 +0900

    出力されたURLのコピー機能を実装

--- app/assets/javascripts/scraping.js
+++ app/assets/javascripts/scraping.js
@@ -4,4 +4,18 @@ $(function() {
     $(this).hide();
     $('.spinner-geturl').show();
   });
+
+  $('.js-copy').on('click', function(){
+    let urls = $(this).parent().find('.js-urltext');
+    let $clipboard = $('<textarea></textarea>');
+    let text = "";
+
+    $.each(urls, function(index, url) { text += $(url).text() + '\n'; });
+    $clipboard.text(text);
+    $(this).append($clipboard);
+    $clipboard.select();
+    document.execCommand('copy');
+    $clipboard.remove();
+    $('.js-copied').show().delay(2000).fadeOut(500);
+  });
 });
